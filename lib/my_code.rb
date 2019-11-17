5bd2d72806221a2f2b2e9e5a2751d28e3268e51e
Jeremy Gray <jeremygray@utexas.edu>
2019-11-17 06:44:12 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -30,7 +30,7 @@ def map_to_square(source_array)
   source_array
 end
 
-def reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array, starting_point*)
   total=0
   if starting_point==nil
     i=0
