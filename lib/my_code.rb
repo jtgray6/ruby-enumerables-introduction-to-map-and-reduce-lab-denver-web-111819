fedcf6299790fc3f3ca4afe576ac0d480ffa4af3
Jeremy Gray <jeremygray@utexas.edu>
2019-11-17 06:49:26 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -31,10 +31,11 @@ def map_to_square(source_array)
 end
 
 def reduce_to_total(source_array, starting_point=0)
-  total=0
-  while starting_point<source_array.length do
-    total=source_array[starting_point]
-    starting_point+=1
+  total=starting_point
+  i=0
+  while i<source_array.length do
+    total=source_array[i]
+    i+=1
   end
   total
 end
