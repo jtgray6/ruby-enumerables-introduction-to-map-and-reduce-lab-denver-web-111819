81500dbeea027ee1c262ecdb00c235e1d6947a8f
Jeremy Gray <jeremygray@utexas.edu>
2019-11-17 06:25:33 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,3 +11,12 @@ end
 def map_to_no_change(source_array)
   source_array
 end
+
+def map_to_double(source_array)
+  i=0
+  while i<source_array.length
+    source_array[i]=source_array[i]*2
+    i+=1
+  end
+  source_array
+end
