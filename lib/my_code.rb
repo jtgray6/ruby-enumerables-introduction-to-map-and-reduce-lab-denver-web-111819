228a7b28afe3afa702f4e0e1dd94bd817d389a23
Jeremy Gray <jeremygray@utexas.edu>
2019-11-17 06:56:34 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -39,3 +39,14 @@ def reduce_to_total(source_array, starting_point=0)
   end
   total
 end
+
+def reduce_to_all_true(source_array)
+  i=0
+  while i<source_array.length
+    if source_array[i]==true
+      true
+    else
+      break
+    end
+  end
+end
